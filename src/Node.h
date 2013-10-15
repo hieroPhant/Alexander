98309fe02e2c78e96e2794279ca954aea8345170
jackhall <jackwhall7@gmail.com>
2013-10-15 17:51:15 -0500

    still drafting new Node

--- src/Node.h
+++ src/Node.h
@@ -8,49 +8,49 @@ namespace alex {
     template<typename LINK>
     class Node {
         class Linker {
-            Node* source, target;
+            //inherit from Head?
+            Head* source, target;
             std::mutex ptr_mutex;
-            std::shared_ptr<Linker> next_input, next_output;
+            Linker* next_input, next_output;
             LINK link;
-            bool ready; //need this? make it atomic?
+            bool ready; 
         };
 
-        std::unique_ptr<Linker> input_head, output_head;
-        std::mutex node_mutex;
+        class Head {
+            std::mutex head_mutex;
+            Linker* first_link;
+            void clean(Head& other);
 
-        void clean(Linker& linker) {
-            auto seek = [](std::shared_ptr<Linker>& linker_ptr) {
-            };
-        }
+        public:
+            Head();
+            Head& Head(const Head& rhs);
+            Head& operator=(const Head& rhs);
+            ~Head();
+
+            template<typename FUNCTION> FUNCTION for_each(FUNCTION f) const;
+        };
+
+        //unique_ptrs would require deletion of copy and assignment
+        struct Links { Head inputs, outputs; };
+        std::shared_ptr<Links> links;
 
     public:
-        Node() = default;
-        Node(const Node& rhs) = delete;
-        Node(Node&& rhs);
-        Node& operator=(const Node& rhs) = delete;
-        Node& operator=(Node&& rhs);
-        ~Node();
+        template<typename FUNCTION>
+        FUNCTION Node::for_inputs(FUNCTION f) const;
+        template<typename FUNCTION>
+        FUNCTION Node::for_outputs(FUNCTION f) const;
 
-        template<typename FUNCTION> FUNCTION for_inputs(FUNCTION f) const;
-        template<typename FUNCTION> FUNCTION for_outputs(FUNCTION f) const;
-
-        bool link(Node& source, LINK& new_link);
-        bool link(Node& source, LINK&& new_link);
-        template<typename... ARGS> bool emplace_link(ARGS... args);
+        template<typename... ARGS> bool link(Node& source, ARGS... args);
         bool mirror(const Node& other);
-        void unlink(Node& source);
         void unlink_all();
-        bool linked_to(const Node& source) const;
-        size_t size() const;
+        
+        bool linked_to(const Node& other) const;
+        bool size() const;
     };
 
-    template<typename LINK> Node<LINK>::Node(Node&& rhs) {}
-    template<typename LINK> Node& Node<LINK>::operator=(Node&& rhs) {}
-    template<typename LINK> Node::~Node() {
-        unlink_all();
-    }
     template<typename LINK> template<typename FUNCTION> 
     FUNCTION Node<LINK>::for_inputs(FUNCTION f) const {
+        
         auto traverse = [f](Linker& linker) {
             f(linker.link);
             if(!linker.next_input) return f;
@@ -67,9 +67,9 @@ namespace alex {
         };
         return traverse(*output_head, f);
     }
-    template<typename LINK> bool Node<LINK>::link(Node& source, LINK& new_link) {}
-    template<typename LINK> bool Node<LINK>::link(Node& source, LINK&& new_link) {}
-    template<typename LINK> template<typename... ARGS> bool Node<LINK>::emplace_link(ARGS... args) {}
+    template<typename LINK> 
+    template<typename... ARGS> 
+    bool Node<LINK>::emplace_link(Node& source, ARGS... args) {}
     template<typename LINK> bool Node<LINK>::mirror(const Node& other) {}
     template<typename LINK> void Node<LINK>::unlink(Node& source) {}
     template<typename LINK> void Node<LINK>::unlink_all() {}
